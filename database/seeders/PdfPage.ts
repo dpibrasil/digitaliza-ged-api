dea177183803f965948b56eda2b99535dff16a73
Toni Moreira <moreiraatoni@gmail.com>
2024-12-20 15:36:06 -0300

    feat: pdf page

---
+++ database/seeders/PdfPage.ts
@@ -0,0 +1,32 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import { countPdfPages } from 'App/Lib/CountPdfPages';
+import { DocumentVersion } from 'App/Models'
+import encrypt from 'node-file-encrypt';
+import fs from 'fs/promises'
+
+export default class extends BaseSeeder {
+  public async run() {
+    const documentVersions = await DocumentVersion.query()
+      .preload('document')
+      .preload('storage')
+      .where('pages', 1)
+    for (const document of documentVersions) {
+      try {
+        const encryptedFilePath = await document.getLocalPath()
+        const encryptedFile = new encrypt.FileEncrypt(encryptedFilePath, `${document.storage.path}/temp`)
+        encryptedFile.openSourceFile()
+        await encryptedFile.decryptAsync(document.document.secretKey)
+        const newPath = encryptedFile.decryptFilePath
+        console.log(newPath)
+        const pageCount = await countPdfPages(newPath)
+        await fs.unlink(newPath)
+
+        document.pages = pageCount
+
+        await document.save()
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+}
